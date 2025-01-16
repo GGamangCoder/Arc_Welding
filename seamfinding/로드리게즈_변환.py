6c986441d4e121b2c875f89821984fe616dae35d
김민재 <zxskmj@gmail.com>
2025-01-16 09:57:04 +0900

    250116 파일 및 코드 정리

--- seamfinding/로드리게즈_변환.py
+++ seamfinding/로드리게즈_변환.py
@@ -42,47 +42,53 @@ def rotation_formula(data, dir):
 
 '''---------------------------------------------------------------------------------------------------------------'''
 
-points = np.loadtxt("./data/3_circle_hole.txt")
-origin_data = points
+def main_func(points):
+    # 데이터 정렬을 위해 첫 점을 원점으로 이동
+    points -= points[0]
 
-points -= points[0]
+    # 진행 방향
+    dir = "X"
+    # 주축에 따라서 축 설정
+    # X 축이면 인덱스 0, Y 축이면 인덱스 1
+    axis_idx = 0 if dir == "X" else 1
+    print(dir, "축: ", axis_idx)
 
-# 진행 방향
-dir = "X"
-# 주축에 따라서 축 설정
-# X 축이면 인덱스 0, Y 축이면 인덱스 1
-axis_idx = 0 if dir == "X" else 1
-print(dir, "축: ", axis_idx)
-
-transformed_data = rotation_formula(points, axis_idx)
+    transformed_data = rotation_formula(points, axis_idx)
 
 
-fig = plt.figure(figsize=(12, 6))
-ax = fig.add_subplot(121, projection='3d')
+    fig = plt.figure(figsize=(12, 6))
+    ax = fig.add_subplot(121, projection='3d')
 
-# -------------------------------------------- 1번 그래프
-ax.scatter(points[:, 0], points[:, 1], points[:, 2], label='transi points', color='g')
-ax.scatter(transformed_data[:, 0], transformed_data[:, 1], transformed_data[:, 2], label='transformed points', color='b')
+    # -------------------------------------------- 1번 그래프
+    ax.scatter(points[:, 0], points[:, 1], points[:, 2], label='Origin points', color='g', s=3)
+    ax.scatter(transformed_data[:, 0], transformed_data[:, 1], transformed_data[:, 2], label='Trans points', color='b', s=5)
 
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
 
-ax.set_title("3d Points & Rotation")
-ax.legend()        # 범례 시각화
+    ax.set_title("3d Points & Rotation")
+    ax.legend()        # 범례 시각화
 
-# -------------------------------------------- 2번 그래프
-ax2 = fig.add_subplot(122)
-ax2.scatter(transformed_data[:, axis_idx], transformed_data[:, 2], label='yz points', color='cyan')     # 주축에 따라서 축 설정
+    # -------------------------------------------- 2번 그래프
+    ax2 = fig.add_subplot(122)
+    ax2_label = f'{dir}Z points'
+    ax2.scatter(transformed_data[:, axis_idx], transformed_data[:, 2], label=ax2_label, s=5)     # 주축에 따라서 축 설정
 
-# ax2.scatter(projected_points[:, 0], projected_points[:, 1], label='Projected Points', s=1)
-# ax2.plot(poly_x, poly_y, color='orange', label='Fitted Polynomial')
-# ax2.scatter(minima[0], minima[1], color='red', label='Minima', s=20)
+    # ax2.scatter(projected_points[:, 0], projected_points[:, 1], label='Projected Points', s=1)
+    # ax2.plot(poly_x, poly_y, color='orange', label='Fitted Polynomial')
+    # ax2.scatter(minima[0], minima[1], color='red', label='Minima', s=20)
 
-ax2.set_xlabel(f'{dir}')
-ax2.set_ylabel('Z')
+    ax2.set_xlabel(f'{dir}')
+    ax2.set_ylabel('Z')
 
-ax2.set_title(f"2D {dir}Z Proj Plane")
-ax2.legend()
+    ax2.set_title(f"2D {dir}Z Proj Plane")
+    ax2.legend()
 
-plt.show()
+    plt.show()
+
+
+if __name__ == "__main__":
+    points = np.loadtxt("./data/3_circle_hole.txt")
+    
+    main_func(points)
